76925d40b8845e6e746f24de52208a8183202a87
mtayllan <messiastayllan@gmail.com>
2023-02-01 21:04:32 -0300

    remove json dependency to avoid runtime error

--- script.rb
+++ script.rb
@@ -1,5 +1,4 @@
 require 'js'
-require 'json'
 
 Item = Data.define(:template, :placeholder_count)
 Document = JS.global[:document]
@@ -14,15 +13,17 @@ UpdateOutputs = lambda do
 end
 
 UpdateStorage = lambda do
-  items_string = Items.map(&:to_h).to_json
-  JS.eval("localStorage.setItem('@items', '#{items_string}')")
+  items_arrays = Items.map { |item| [item.template, item.placeholder_count] }.transpose
+  templates_string = items_arrays[0].join('||')
+  placeholder_counts_string = items_arrays[1].join('||')
+  JS.eval("localStorage.setItem('@items', '#{templates_string},#{placeholder_counts_string}')")
 end
 
 DeleteItem = lambda do |item, row|
   row.remove
   Items.delete(item)
-  UpdateOutputs.()
-  UpdateStorage.()
+  UpdateOutputs.call
+  UpdateStorage.call
 end
 
 RenderItem = lambda do |item|
@@ -38,7 +39,7 @@ RenderItem = lambda do |item|
   delete_el = Document.createElement('td')
   delete_btn = Document.createElement('button')
   delete_btn[:innerText] = 'delete'
-  delete_btn.addEventListener('click', ->(_) { DeleteItem.(item, row_el) })
+  delete_btn.addEventListener('click', ->(_) { DeleteItem[item, row_el] })
   delete_el.appendChild(delete_btn)
 
   row_el.appendChild(template_el)
@@ -61,42 +62,44 @@ AddItem = lambda do
   placeholder_count_input[:value] = ''
 
   Items.push(item)
-  RenderItem.(item)
-  UpdateOutputs.()
-  UpdateStorage.()
+  RenderItem[item]
+  UpdateOutputs.call
+  UpdateStorage.call
+end
+
+LoadFromStrings = lambda do |templates_string, placeholder_counts_string|
+  template_list = templates_string.split('||')
+  placeholder_count_list = placeholder_counts_string.split('||')
+
+  template_list.each_with_index do |template, index|
+    item_data = Item.new(template, placeholder_count_list[index])
+    Items.push(item_data)
+    RenderItem[item_data]
+  end
 end
 
 items_string = JS.eval("return localStorage.getItem('@items')")
 
 if items_string != JS::Undefined
-  JSON.parse(items_string.to_s).each do |item|
-    item_data = Item.new(item['template'], item['placeholder_count'])
-    Items.push(item_data)
-    RenderItem.(item_data)
-  end
+  template_list, placeholder_count_list = items_string.to_s.split(',')
+  LoadFromStrings[template_list, placeholder_count_list]
 end
 
-UpdateOutputs.()
+UpdateOutputs.call
 
 add_button = Document.getElementById('add-button')
 add_button.addEventListener('click', ->(_) { AddItem.call })
 
 load_button = Document.getElementById('load-button')
 load_button.addEventListener('click', lambda do |_|
-  template_list_input = Document.getElementById('template-list-input')
-  placeholder_count_list_input = Document.getElementById('placeholder-count-list-input')
-
-  template_list = template_list_input[:value].to_s.split('||')
-  placeholder_count_list = placeholder_count_list_input[:value].to_s.split('||')
-
   Items.clear
   Document.getElementById('table-body')[:innerText] = ''
 
-  template_list.each_with_index do |template, index|
-    item_data = Item.new(template, placeholder_count_list[index])
-    Items.push(item_data)
-    RenderItem.(item_data)
-  end
-  UpdateStorage.()
-  UpdateOutputs.()
+  template_list = Document.getElementById('template-list-input')[:value].to_s
+  placeholder_count_list = Document.getElementById('placeholder-count-list-input')[:value].to_s
+
+  LoadFromStrings[template_list, placeholder_count_list]
+
+  UpdateStorage.call
+  UpdateOutputs.call
 end)
